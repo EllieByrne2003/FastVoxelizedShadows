6133302f09131ef8fc9576d306563251be3893bc
EllieByrne2003 <leonbyrne52@gmail.com>
2026-03-10 18:38:46 +0000

    Some changes to scenes.

--- src/scene/scene.cpp
+++ src/scene/scene.cpp
@@ -45,6 +45,16 @@ Scene * Scene::readScene(const json &jsonScene) {
         }
     }
 
+    if(jsonScene.contains("lights")) {
+        for(const json &jsonLight : jsonScene["lights"]) {
+            Light *light = Light::readLight(jsonLight);
+
+            if(light != nullptr) {
+                scene->addLight(light);
+            }
+        }
+    }
+
     return scene;
 }
 
@@ -57,6 +67,14 @@ Scene * Scene::readScene(const std::string &sceneName) {
 }
 
 void Scene::draw() {
+    Renderer::getInstance().setView(camera.get_view_matrix());
+
+    for(Model *model : models) {
+        model->draw(this);
+    }
+}
+
+void Scene::drawDepths(const bool drawEntry) {
     for(Model *model : models) {
         model->draw();
     }
@@ -64,4 +82,32 @@ void Scene::draw() {
 
 void Scene::addModel(Model *model) {
     models.push_back(model);
+}
+
+void Scene::addLight(Light *light) {
+    lights.push_back(light);
+}
+
+void Scene::move(const Direction direction, const float time) {
+    camera.move(direction, time);
+
+    Renderer::getInstance().setView(camera.get_view_matrix());
+}
+
+void Scene::look(const float deltaX, const float deltaY) {
+    camera.look(deltaX, deltaY);
+
+    Renderer::getInstance().setView(camera.get_view_matrix());
+}
+
+vec3 Scene::getCameraPos() const {
+    return camera.getPos();
+}
+
+// TODO temporary
+mat4 Scene::getLightMatrix() const {
+    const mat4 proj = glm::ortho(0.0f, 1024.0f, 0.0f, 1024.0f, 0.1f, 1000.0f);
+    const mat4 view = lights[0]->get_view_matrix();
+
+    return proj * view;
 }
