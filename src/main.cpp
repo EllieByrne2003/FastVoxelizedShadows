ca30cbfe2b3539754f7c054dacba518f3a3c288f
EllieByrne2003 <leonbyrne52@gmail.com>
2026-03-10 18:40:58 +0000

    Idk.

--- src/main.cpp
+++ src/main.cpp
@@ -64,7 +64,7 @@ int main() {
     do {
         glClearColor(0.3f, 0.0f, 0.5f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-        
+
         // Get movement
         processKey(scene, window);
 
